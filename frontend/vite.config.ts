54ec4aabc1386d9e453b37db7237071353fe9100
mohamed5828 <mohamed.ahmed5828@gmail.com>
2024-08-07 06:38:30 +0300

    added two method in fronend one for post and other for fetch

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { config } from "dotenv";
 
 // https://vitejs.dev/config/
+config({ path: "./env" });
+
 export default defineConfig({
   plugins: [react()],
-})
+});
