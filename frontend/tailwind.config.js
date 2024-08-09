129d1d753d50466d827493a4869f55aaf2adf2c5
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-08-09 09:42:29 +0300

    tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,21 +3,15 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tea-green': '#ccd5ae',
+        'beige': '#e9edc9',
+        'corn-silk': '#fefae0',
+        'papaya-whip': '#faedcd',
+        'buff': '#d4a373',
+      },
+    }
   },
   plugins: [],
 };
-
-module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                'tea-green': '#CCD5AE',
-                'beige': '#E9EDC9',
-                'corn-silk': '#FEFAE0',
-                'papaya-whip': '#FAEDCD',
-                'buff': '#D4A373',
-            },
-        }
-    }
-}
