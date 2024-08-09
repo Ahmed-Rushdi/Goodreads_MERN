f8e5af868111267460aa8ce240fee3611e097c2f
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-08-09 09:26:22 +0300

    tailwind config color theme

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'tea-green': '#CCD5AE',
+                'beige': '#E9EDC9',
+                'corn-silk': '#FEFAE0',
+                'papaya-whip': '#FAEDCD',
+                'buff': '#D4A373',
+            },
+        }
+    }
+}
