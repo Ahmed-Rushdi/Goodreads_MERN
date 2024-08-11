a0c4fd56104b16fdd7ac33712c99d73db7d6cf31
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-08-11 03:12:02 +0300

    refactored the router | alot of work to be done going to sleep

--- backend/src/routes/BooksRoute.js
+++ backend/src/routes/BooksRoute.js
@@ -33,35 +33,6 @@ router.delete("/:isbn13", async (req, res) => {
   res.send(book);
 });
 
-// * Reviews
-// TODO: Add more operations for reviews
-router.get("/:isbn13/reviews", async (req, res) => {
-  const book = await Book.findOne({ isbn13: req.params.isbn13 });
-  if (!book) return res.status(404).send("Book not found");
-  const reviews = book.reviews;
-  res.send(reviews);
-});
-
-router.post("/:isbn13/reviews", async (req, res) => {
-  const book = await Book.findOne({ isbn13: req.params.isbn13 });
-  if (!book) return res.status(404).send("Book not found");
-  const review = new Review(req.body);
-  book.reviews.push(review);
-  await book.save();
-  res.send(review);
-});
-
-// FIXME : implement user side deletion
-// TODO: possibly change the endpoint review id to user id and pop the review from the books and the
-router.delete("/:isbn13/reviews/:reviewId", async (req, res) => {
-  const book = await Book.findOne({ isbn13: req.params.isbn13 });
-  if (!book) return res.status(404).send("Book not found");
-  const review = book.reviews.id(req.params.reviewId);
-  if (!review) return res.status(404).send("Review not found");
-  review.remove();
-  await book.save();
-  res.send(review);
-});
 
 // TODO: Test and add more operations
 
---
+++ backend/src/routes/ReviewsRoute.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const Book = require("../models/Book.model");
+const Review = require("../models/Review.model");
+// const User = require("../models/User.model");
+
+const router = express.Router();
+
+// * Reviews
+// TODO: integrate with user  (uncomment code when the user model is available)
+
+// retrieve all reviews of specific book
+router.get("/book/:isbn13", async (req, res) => {
+  const book = await Book.findOne({ isbn13: req.params.isbn13 });
+  if (!book) return res.status(404).send("Book not found");
+  const reviews = book.reviews;
+  res.send(reviews);
+});
+
+// retrieve all reviews of specific book
+router.get("/user/:userId", async (req, res) => {
+  // TODO: integrate with user model (uncomment code when the user model is available)
+  //   const user = await User.findOne({ _id: req.params.userId });
+  //   if (!user) return res.status(404).send("User not found");
+  //   const reviews = user.reviews;
+  //   res.send(reviews);
+});
+
+// add review to specific book and user
+router.post("/:isbn13", async (req, res) => {
+  const book = await Book.findOne({ isbn13: req.params.isbn13 });
+  if (!book) return res.status(404).send("Book not found");
+  // TODO: integrate with user model
+  const review = new Review(req.body);
+  book.reviews.push(review);
+  await book.save();
+  res.send("review added");
+});
+
+// TODO: integrate with user model (uncomment code when the user model is available)
+// delete specific review from book and user
+router.delete("/:isbn13/:userId", async (req, res) => {
+  const book = await Book.findOne({
+    isbn13: req.params.isbn13,
+  });
+  if (!book) return res.status(404).send("Book not found");
+  //   const user = await User.findOne({ _id: req.params.userId });
+  //   if (!user) return res.status(404).send("Review not found");
+  const reviewPartial = {
+    bookId: req.params.isbn13,
+    userId: req.params.userId,
+  };
+  book.reviews.pull(reviewPartial);
+  //   user.reviews.pull(reviewPartial);
+  await book.save();
+  await user.save();
+  res.send("review deleted");
+});
