0ba59318beda41ff8d5f77350f3db086bf88fe73
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-08-11 21:56:38 +0300

    quick fix for User.model.js

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,16 +1,17 @@
-import mongoose from "mongoose";
+const { Schema, model } = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   userRole: { type: String, enum: ["user", "admin"], default: "user" },
-  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
-  books: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
-  currentlyReading: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
-  wantToRead: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+  books: [{ type: Schema.Types.ObjectId, ref: "Book" }],
+  currentlyReading: [{ type: Schema.Types.ObjectId, ref: "Book" }],
+  wantToRead: [{ type: Schema.Types.ObjectId, ref: "Book" }],
   verificationToken: { type: String },
   refreshToken: { type: String },
 });
 
-export default mongoose.model("User", userSchema);
+const User = model("User", userSchema);
+module.exports = User;
