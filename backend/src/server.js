5c7db86a743a5fc8cc66518fc04b193f687e9465
Hossam Elbatal <tokyo.hossam@gmail.com>
2024-08-13 00:29:16 +0300

    added db connection to separate file | changed server connection function

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,32 +2,43 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-
+const process = require("process");
 const booksRoute = require("./routes/BooksRoute");
 const reviewsRoute = require("./routes/ReviewsRoute");
+const userRoutes = require("./routes/UserRoute");
+const connectDB = require("./utils/dbConnection");
 
 const port = process.env.PORT;
 const mongoUri = process.env.MONGODB_URI;
 const logging = process.env.LOGGING;
 
 const app = express();
-
+// registeration !!!!
 // * Expose public for thumbnail retrieval (host:port/thumbnails)
 // ? is this the way to do it? IDK.
 app.use(express.static(path.join(__dirname, "../public")));
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB connected"))
-  .then(() => console.log("Database Name:", mongoose.connection.name))
-  .catch((err) => console.log(err));
-
 app.use(express.json());
-app.use(logger(logging));
 
+app.use(logger(logging));
+app.use("/api/users", userRoutes);
 app.use("/api/books", booksRoute);
 app.use("/api/reviews", reviewsRoute);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://0.0.0.0:${port}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(3000, () => {
+      console.log("Server is running on http://0.0.0.0:${port}");
+    });
+  } catch (error) {
+    console.error("Failed to start server:", err.message);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+// connectDB();
+// app.listen(port, () => {
+//   console.log(`Server is running on http://0.0.0.0:${port}`);
+// });
