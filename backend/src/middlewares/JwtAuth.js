edb4318121f6abd8a3b7d866a7e9f25267016ed3
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-08-14 01:31:15 +0300

    changed jwt middleware to recive token from cookies not header | added author and categ routes to server

--- backend/src/middlewares/JwtAuth.js
+++ backend/src/middlewares/JwtAuth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User.model");
 
 const userAuth = (req, res, next) => {
-  const token = req.headers["x-access-token"];
+  const token = req.cookies?.jwt;
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
@@ -20,7 +20,7 @@ const adminAuth = async (req, res, next) => {
     const userId = req.user?.id;
     if (!userId) return res.status(403).send("Unauthorized");
     const user = await User.findById(userId).select("role");
-    if (!user || user.role !== "admin")
+    if (!user || user.userRole !== "admin")
       return res.status(403).send("Unauthorized");
     return next();
   } catch (error) {
