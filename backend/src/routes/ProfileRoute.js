8380a6420eac7d1bf3dea0b09b85e573fe4d34a5
Abram Naguib <engabram.93@gmail.com>
2024-08-14 13:41:55 +0300

    Fifth Commit: Updating the backend to ensure connection with the database

--- backend/src/routes/ProfileRoute.js
+++ backend/src/routes/ProfileRoute.js
@@ -4,6 +4,6 @@ const { getAllBooks } = require('../controllers/bookControllers')
 
 const router = express.Router();
 
-router.get('/api/profile', getAllBooks);
+router.get('/', getAllBooks);
 
 module.exports = router;
