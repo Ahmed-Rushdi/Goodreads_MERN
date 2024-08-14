13439c841234713b9737f122e9059902010d151f
mohamed5828 <mohamed.ahmed5828@gmail.com>
2024-08-14 19:41:08 +0300

    stable prototype for the trendings

--- backend/src/controllers/trendsControllers.js
+++ backend/src/controllers/trendsControllers.js
@@ -1,6 +1,6 @@
 const Book = require("../models/Book.model");
 const Author = require("../models/Author.model");
-const Category = require("../models/Author.model");
+const Category = require("../models/Category.model");
 const getTrendingBooks = async (req, res) => {
   try {
     const trendingBooks = await Book.find({})
@@ -13,26 +13,72 @@ const getTrendingBooks = async (req, res) => {
 };
 const getTrendingAuthors = async (req, res) => {
   try {
-    const trendingAuthors = await Author.find({})
-      .populate("bookCount")
-      .populate("totalRatings")
-      .populate("averageRating")
-      .sort({ totalRating: -1, averageRating: -1 })
-      .limit(10);
+    const trendingAuthors = await Author.aggregate([
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "authorId",
+          as: "books",
+        },
+      },
+      {
+        $unwind: "$books",
+      },
+      {
+        $group: {
+          _id: "$_id",
+          name: { $first: "$name" },
+          bio: { $first: "$bio" },
+          birthDate: { $first: "$birthDate" },
+          totalRatings: { $sum: "$books.ratingsCount" },
+          averageRating: { $avg: "$books.averageRating" },
+        },
+      },
+      {
+        $sort: { totalRatings: -1, averageRating: -1 },
+      },
+      {
+        $limit: 10,
+      },
+    ]);
 
     res.status(200).json(trendingAuthors);
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error fetching trending authors ", error });
+    res.status(500).json({ message: "Error fetching trending authors", error });
   }
 };
 const getTrendingCategories = async (req, res) => {
   try {
-    const trendingCategories = await Category.find({})
-      .populate("bookCount")
-      .sort({ bookCount: -1 })
-      .limit(10);
+    const trendingCategories = await Category.aggregate([
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "categories",
+          as: "books",
+        },
+      },
+      {
+        $addFields: {
+          bookCount: { $size: "$books" },
+        },
+      },
+      {
+        $sort: { bookCount: -1 },
+      },
+      {
+        $limit: 10,
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          bookCount: 1,
+        },
+      },
+    ]);
 
     res.status(200).json(trendingCategories);
   } catch (error) {
@@ -41,7 +87,6 @@ const getTrendingCategories = async (req, res) => {
       .json({ message: "Error fetching trending categories ", error });
   }
 };
-
 module.exports = {
   getTrendingBooks,
   getTrendingAuthors,
--- backend/src/models/Category.model.js
+++ backend/src/models/Category.model.js
@@ -9,25 +9,5 @@ const categorySchema = new Schema({
   description: { type: String },
 });
 
-categorySchema.virtual("bookCount", {
-  ref: "Book",
-  localField: "_id",
-  foreignField: "categories",
-  count: true,
-});
-
 const Category = model("Category", categorySchema);
 module.exports = Category;
-// const { Schema, model } = require("mongoose");
-
-// const categorySchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   description: { type: String },
-// });
-
-// const Category = model("Category", authorSchema);
-// module.exports = Category;
