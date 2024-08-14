f5923b50296b8fe99180d225f6d2006983653d9b
Hossam Elbatal <tokyo.hossam@gmail.com>
2024-08-14 21:51:54 +0300

    added custom hook to get the jwt

--- backend/src/server.js
+++ backend/src/server.js
@@ -56,7 +56,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// app.use("/api/auth", authenticationRoute);
+app.use("/api/auth", authenticationRoute);
 
 app.use(logger(logging));
 app.use("/api/users", userRoutes);
