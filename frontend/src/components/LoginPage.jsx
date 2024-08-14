837c1476e1239c68badc4e6feb28cac5a1607e42
Hossam Elbatal <tokyo.hossam@gmail.com>
2024-08-14 18:48:16 +0300

    added routes and fixed styling issues

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -6,7 +6,8 @@ const LoginPage = () => {
   const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault();
+
     // Handle form submission logic here
     console.log("Sign In:", { email, password });
   };
@@ -17,8 +18,8 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="cont">
-      <form className="form sign-in" onSubmit={handleSubmit}>
+    <div className="cont-1">
+      <form className="form-1 sign-in" onSubmit={handleSubmit}>
         <h2>Welcome back,</h2>
         <label>
           <span>Email</span>
@@ -39,21 +40,21 @@ const LoginPage = () => {
         <a className="forgot-pass" href="#">
           Forgot password?
         </a>
-        <button type="submit" className="submit">
+        <button type="submit" className="submit-1">
           Sign In
         </button>
-        <button type="button" className="fb-btn" onClick={handleGmailConnect}>
+        <button type="button" className="fb-btn-1" onClick={handleGmailConnect}>
           Connect with <span>Gmail</span>
         </button>
-        <button className="sign-up">Sign Up</button>
+        <button className="sign-up-1">Sign Up</button>
       </form>
 
       <div className="sub-cont">
-        <div className="img">
+        <div className="img-1">
           <div className="img__text m--up">
             <h1 className="new-here">New here?</h1>
             <h2>Sign up now!</h2>
-            <button className="sign-up-btn">Sign Up</button>
+            <button className="sign-up-btn-1">Sign Up</button>
           </div>
         </div>
       </div>
