e4c971012f1b240aa391283ba3c4df0c8490e67e
mohamed5828 <mohamed.ahmed5828@gmail.com>
2024-08-22 14:23:48 +0300

    gotfix for decod env

--- backend/src/middlewares/JwtAuth.js
+++ backend/src/middlewares/JwtAuth.js
@@ -7,7 +7,7 @@ const userAuth = (req, res, next) => {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
   } catch (err) {
     return res.status(401).send("Invalid Token");
