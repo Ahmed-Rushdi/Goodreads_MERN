229b6b8d013f7b913f8101f0d32eb68a3163bd9e
mohamed5828 <mohamed.ahmed5828@gmail.com>
2024-09-02 15:09:20 +0300

    hotfix

--- backend/src/server.js
+++ backend/src/server.js
@@ -47,7 +47,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://musical-muffin-2bd569.netlify.app", // Your React frontend URL
+    origin: [
+      "https://musical-muffin-2bd569.netlify.app", // Your production React frontend URL
+      "http://localhost:5173", // Your local development React frontend URL
+    ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // If you want to allow cookies or authentication headers
   })
